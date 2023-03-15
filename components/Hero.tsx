53d2b8ac3455788c99686bdc1aeee20cc38932b9
Magmastorm <shashwat.nath@hotmail.com>
2023-03-16 01:57:29 +0530

    about

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,15 +19,15 @@ const Hero = () => {
     <div className="flex flex-col items-center justify-center">
       
       <div className="flex flex-col items-center justify-center tracking-wider">
-        <h1 className="pb-4 text-5xl font-bold font-kaushan">
+        <h1 className="pb-3  text-5xl ">
           Shashwat Nath
         </h1>
-        <h3 className="pb-4 text-2xl font-bold font-kaushan">
+        <h3 className="pb-3 text-2xl font-bold ">
           MERN Stack Developer
         </h3>
       </div>
       <div className="p-2">
-        <button onClick={handlescroll}className="p-2 w-[180px] text-xl font-kaushan">
+        <button onClick={handlescroll}className="p-2 w-[180px] text-xl ">
           Scroll
         </button>
       </div>
